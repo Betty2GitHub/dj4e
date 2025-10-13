deaba555d69ce43d04311cbe1aef2b426c65ae12
Betty Huang <bettyhuang@BettydeMacBook-Air.local>
2025-10-12 23:07:18 -0400

    complete solo1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [ '*' ]
 
 INSTALLED_APPS = [
     "polls.apps.PollsConfig",
+    "solo1.apps.Solo1Config",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
