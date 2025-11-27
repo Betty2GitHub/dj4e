c9f036feac397c2038c58652929b06ab1d75970e
Esther_Xinyi <xinyih@umich.edu>
2025-11-27 16:49:00 -0500

    Marketplace comments Milestone 3

--- market/mkt/views.py
+++ market/mkt/views.py
@@ -1,12 +1,12 @@
 from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from mkt.models import Ad
+from mkt.models import Ad, Comment
 from mkt.owner import OwnerListView, OwnerDetailView, OwnerDeleteView
-from mkt.forms import CreateForm
+from mkt.forms import CreateForm, CommentForm
 
 class AdListView(OwnerListView):
     model = Ad
@@ -15,6 +15,13 @@ class AdListView(OwnerListView):
 
 class AdDetailView(OwnerDetailView):
     model = Ad
+    template_name = "mkt/ad_detail.html"
+    def get(self, request, pk) :
+        x = get_object_or_404(Ad, id=pk)
+        comments = Comment.objects.filter(ad=x).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = { 'ad' : x, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
 
 class AdCreateView(LoginRequiredMixin, View):
     template_name = 'mkt/ad_form.html'
@@ -63,6 +70,22 @@ class AdUpdateView(LoginRequiredMixin, View):
 
 class AdDeleteView(OwnerDeleteView):
     model = Ad
+
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        f = get_object_or_404(Ad, id=pk)
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=f)
+        comment.save()
+        return redirect(reverse('mkt:detail', args=[pk]))
+
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+    template_name = "mkt/comment_delete.html"
+
+    # https://stackoverflow.com/questions/26290415/deleteview-with-a-dynamic-success-url-dependent-on-id
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse('mkt:detail', args=[ad.id])
 
 def stream_file(request, pk):
     ad = get_object_or_404(Ad, id=pk)
